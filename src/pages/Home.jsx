938dd611580c8faa70f6bf52220efc7400c4ace6
Ayushgit19 <146693918+Ayushgit19@users.noreply.github.com>
2025-08-14 20:50:57 +0530

    Portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen '>
+
+        {/* Theme Toggler */}
+
+
+        {/* Background Effects */}
+
+
+        {/* NavBar */}
+
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+
+    </div>
+  )
+}
+
+export default Home
